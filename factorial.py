a32a2afd63a2953c58717d666319e9baf21581b5
sribalakrish14 <46369019+sribalakrish14@users.noreply.github.com>
2019-02-01 09:02:30 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
-num=int(input())
-if num==0 or num==1:
+n=int(input())
+if n==0 or n==1:
     print "1"
 else:
     fact=1
-    for i in range (1,num+1):
+    for i in range (1,n+1):
         fact=fact*i
         i+=1
     print fact   
