1e998ef480b64280d5eb23566dc0a1e3fefe5341
sribalakrish14 <46369019+sribalakrish14@users.noreply.github.com>
2019-02-01 09:48:49 +0530

    Create date.py

---
+++ date.py
@@ -0,0 +1,5 @@
+day=raw_input()
+if day=="Saturday" or day=="Sunday":
+    print "yes"
+elif (day=="Monday" or day=="Tuesday" or day=="Wednesday" or day=="Thursday" or days=="Friday"):
+    print "no"
